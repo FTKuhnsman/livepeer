22947895a4e673be1d07b2cf4d7e83f9b7926ee6
jkuhnsman <91159572+jkuhnsman@users.noreply.github.com>
2021-10-31 18:51:48 -0400

    Update unregister.py

--- unregister.py
+++ unregister.py
@@ -13,4 +13,9 @@ print = functools.partial(print, flush=True)
 #%%
     
 if __name__ == "__main__":
+    with open('client_config') as f:
+        t = f.read()
+        f.close()
+        ts = t.split('\n')
+        
     r = requests.post('http://{}:{}/'.format(ts[2],ts[3]), json={'command':'unregister','ipAddr':ts[1]})
