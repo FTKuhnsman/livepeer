b06bb46cd63ca15c5a14b54e9ad647e1eb7ecae2
Joey <kuhnsmanj@me.com>
2022-04-16 15:16:25 -0400

    sdfl

--- setFaceValue.py
+++ setFaceValue.py
@@ -1,7 +1,12 @@
 import requests
+import time
 
-files = {
-    'facevaluelimit': (None, '3000000000000000'),
-}
-
-response = requests.post('http://127.0.0.1:7935/setFaceValueLimit', files=files)
+while True:
+    
+    files = {
+        'facevaluelimit': (None, '3000000000000000'),
+    }
+    
+    response = requests.post('http://127.0.0.1:7935/setFaceValueLimit', files=files)
+    
+    time.sleep(60)
